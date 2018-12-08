8a0c4057eb5f6c5edf34001d18e415adfafd4911
moyumin <moyumin@simuwang.com>
2018-12-08 22:19:39 +0800

    重整项目

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,31 @@
-/**
- * @author moyumin
- */
-let gulp = require('gulp');
-let livereload = require('gulp-livereload');
-let sass = require('gulp-sass');
-let babel = require('gulp-babel');
-let shelljs = require('shelljs')
-let uglify = require('gulp-uglify');
-let clean = require('gulp-clean');
-let named = require('vinyl-named');
-let webpackConfig = require('./gulp-config/webpack.dev.conf.js')
-let compiler = require('webpack')
-let webpackStraem = require('webpack-stream')
-let rootDir = '../..';
-
-// 编译项目下的html
-gulp.task('html', () => {
-  gulp.src('src/**/*.html')
-    .pipe(gulp.dest('dist'));
-})
-// 编译项目下的images
-gulp.task('images', () => {
-  gulp.src('src/**/images/*.*')
-    .pipe(gulp.dest('dist'));
-})
-// 编译项目下的scss
-gulp.task('sass', () => {
-  gulp.src('src/**/*.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'           // 配置输出方式,默认为nested
-    }))
-    .pipe(gulp.dest('dist'));
-})
-
-// 编译项目下的javascript
-gulp.task('script', () => {
-  let distPath=''
-  gulp.src('src/**/index.js')
-    .pipe(named(function(file) {
-      let curPath = file.path
-      curPath = curPath.substr(0,curPath.lastIndexOf("\\"))
-      distPath = curPath.replace('src', 'dist')
-      return 'index'
-    }))
-    .pipe(webpackStraem(webpackConfig, compiler, function(err, stats) {
-      /* Use stats to do more things if needed */
-    }))
-    .pipe(gulp.dest(function() {
-      return distPath
-    }))
-})
-// 清理
-gulp.task('clean', function() {
-  return gulp.src('dist/*.*', {read: false})
-    .pipe(clean());
-});
-gulp.task('default', ['clean'],function () {
-  gulp.start('html','images','sass', 'script');
-  shelljs.exec('gulp watch')
-});
-
-gulp.task('watch', function() {
-  livereload.listen();
-  // 看守所有.scss档
-  gulp.watch('src/**/*.scss', ['sass']);
-
-  // 看守所有.js档
-  gulp.watch('src/**/index.js', ['script']);
-
-  // 看守所有图片档
-  gulp.watch('src/**/images/*.*', ['images']);
-
-  //看守html
-  gulp.watch('src/**/*.html', ['html']) ;
-
-
-  gulp.watch(['dist/**']).on('change', (event) => {
-    livereload.changed(event.path)
-  });
+var gulp = require('gulp');
+// 调用 .create() 意味着你得到一个唯一的实例并允许您创建多个服务器或代理。
+var browserSync = require('browser-sync').create();
+// 这里reload不加括号，只引用不调用
+// var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
+gulp.task('server', function() {
+    nodemon({
+        script: 'app.js',
+        // 忽略部分对程序运行无影响的文件的改动，nodemon只监视js文件，可用ext项来扩展别的文件类型
+        ignore: ["gulpfile.js", "node_modules/", "public/**/*.*"],
+        env: {
+            'NODE_ENV': 'development'
+        }
+    }).on('start', function() {
+        browserSync.init({
+            proxy: 'http://localhost:3000',
+            files: ["public/**/*.*", "views/**", "routes/**","index.html"],
+            // 在不同浏览器上镜像点击、滚动和表单，即所有浏览器都会同步
+            ghostMode: {
+                clicks: true,
+                scroll: true
+            },
+            //停止自动打开浏览器
+            open: false,
+            port:8080
+        }, function() {
+            console.log("browser refreshed.");
+        });
+    });
 });
