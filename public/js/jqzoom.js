eeb5f92815eb1665b89259adc3ea6ce50796c094
moyumin <moyumin@simuwang.com>
2019-04-12 17:55:46 +0800

    复杂表单计算

--- public/js/jqzoom.js
+++ public/js/jqzoom.js
@@ -6,51 +6,51 @@
 		yzoom: 200,		//zoomed div default width
 		offset: 10,		//zoomed div default offset
 		position: "right"  //zoomed div default position,offset position is to the right of the image
-	};
+	}
 	if(options) {
-		$.extend(settings, options);
+		$.extend(settings, options)
 	}
 	$(this).hover(function(){
-		var imageLeft = $(this).get(0).offsetLeft;
-		var imageTop =  $(this).get(0).offsetTop;
-		var imageWidth = $(this).get(0).offsetWidth;
-		var imageHeight = $(this).get(0).offsetHeight;
-		var bigimage = $(this).parent().attr("href");
+		var imageLeft = $(this).get(0).offsetLeft
+		var imageTop =  $(this).get(0).offsetTop
+		var imageWidth = $(this).get(0).offsetWidth
+		var imageHeight = $(this).get(0).offsetHeight
+		var bigimage = $(this).parent().attr("href")
 		if($("span.zxx_image_zoom_div").get().length == 0){
-			$(this).after("<span class='zxx_image_zoom_div'><img class='bigimg' src='"+bigimage+"'/></span>");
+			$(this).after("<span class='zxx_image_zoom_div'><img class='bigimg' src='"+bigimage+"'/></span>")
 		}
 		if(settings.position == "right"){
-			leftpos = imageLeft + imageWidth + settings.offset;
+			leftpos = imageLeft + imageWidth + settings.offset
 		}else{
 			// 在左边
-			leftpos = imageLeft - settings.xzoom - settings.offset;
+			leftpos = imageLeft - settings.xzoom - settings.offset
 		}
-		$("span.zxx_image_zoom_div").css({ top: imageTop,left: leftpos });
-		$("span.zxx_image_zoom_div").width(settings.xzoom);
-		$("span.zxx_image_zoom_div").height(settings.yzoom);
-		$("span.zxx_image_zoom_div").show();
+		$("span.zxx_image_zoom_div").css({ top: imageTop, left: leftpos })
+		$("span.zxx_image_zoom_div").width(settings.xzoom)
+		$("span.zxx_image_zoom_div").height(settings.yzoom)
+		$("span.zxx_image_zoom_div").show()
 		$(document.body).mousemove(function(e){
-			var bigwidth = $(".bigimg").get(0).offsetWidth;
-			var bigheight = $(".bigimg").get(0).offsetHeight;
-			var scaley ='x';
-			var scalex= 'y';
+			var bigwidth = $(".bigimg").get(0).offsetWidth
+			var bigheight = $(".bigimg").get(0).offsetHeight
+			var scaley ='x'
+			var scalex= 'y'
 			if(isNaN(scalex)|isNaN(scaley)){
-				var scalex = Math.round(bigwidth/imageWidth) ;
-				var scaley = Math.round(bigheight/imageHeight);
+				var scalex = Math.round(bigwidth/imageWidth) 
+				var scaley = Math.round(bigheight/imageHeight)
 			}
-			mouse = new MouseEvent(e);
-            scrolly = mouse.y - imageTop - ($("span.zxx_image_zoom_div").height()*1/scaley)/2 ;
-			$("span.zxx_image_zoom_div").get(0).scrollTop = (scrolly) * scaley - 64;
-			scrollx = mouse.x - imageLeft - ($("span.zxx_image_zoom_div").width()*1/scalex)/2 ;
-			$("span.zxx_image_zoom_div").get(0).scrollLeft = (scrollx) * scalex;
-		});
-		},function(){
-		   $("span.zxx_image_zoom_div").hide();
-		   $(document.body).unbind("mousemove");
-		   $("span.zxx_image_zoom_div").remove();
-		});
+			mouse = new MouseEvent(e)
+            scrolly = mouse.y - imageTop - ($("span.zxx_image_zoom_div").height()*1/scaley)/2 
+			$("span.zxx_image_zoom_div").get(0).scrollTop = (scrolly) * scaley - 64
+			scrollx = mouse.x - imageLeft - ($("span.zxx_image_zoom_div").width()*1/scalex)/2 
+			$("span.zxx_image_zoom_div").get(0).scrollLeft = (scrollx) * scalex
+		})
+		}, function(){
+		   $("span.zxx_image_zoom_div").hide()
+		   $(document.body).unbind("mousemove")
+		   $("span.zxx_image_zoom_div").remove()
+		})
 	}
-})(jQuery);
+})(jQuery)
 
 function MouseEvent(e) {
 this.x = e.pageX
