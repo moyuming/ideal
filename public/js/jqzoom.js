5c47b273c7f03846814b67098714ab2785ee8bb0
moyumin <moyumin@simuwang.com>
2019-04-18 11:10:47 +0800

    电商图片放大实例优化

--- public/js/jqzoom.js
+++ public/js/jqzoom.js
@@ -10,22 +10,24 @@
       $.extend(settings, options)
     }
     $(this).hover(function () {
-      var imageLeft = $(this).get(0).offsetLeft
-      var imageTop = $(this).get(0).offsetTop
-      var imageWidth = $(this).get(0).offsetWidth
-      var imageHeight = $(this).get(0).offsetHeight
+      console.log($(this).width())
       var imageStyle = document.defaultView.getComputedStyle(this)
       var imageBorderTopWidth = parseInt(imageStyle.borderTop.replace('px', ''))
       var imageBorderLeftWidth = parseInt(imageStyle.borderLeft.replace('px', ''))
       var imagePaddingTopWidth = parseInt(imageStyle.paddingTop.replace('px', ''))
       var imagePaddingLeftWidth = parseInt(imageStyle.paddingLeft.replace('px', ''))
       var bigimage = $(this).parent().attr("href")
+      var imageLeft = $(this).get(0).offsetLeft
+      var imageTop = $(this).get(0).offsetTop
+      var imageOffsetWidth = $(this).get(0).offsetWidth
+      var imageWidth = $(this).width()
+      var imageHeight = $(this).height()
       if ($("span.zxx_image_zoom_div").get().length == 0) {
         $(this).after("<span class='zxx_image_zoom_div'><img class='bigimg' src='" + bigimage + "'/></span>")
       }
       // 左边偏移
       if (settings.position == "right") {
-        leftpos = imageLeft + imageWidth + settings.offset
+        leftpos = imageLeft + imageOffsetWidth + settings.offset
       } else {
         // 在左边
         leftpos = imageLeft - settings.xzoom - settings.offset
@@ -36,20 +38,20 @@
       $("span.zxx_image_zoom_div").show()
       $(document.body).mousemove(function (e) {
         // 计算宽高和倍数
-        var bigwidth = $(".bigimg").get(0).offsetWidth
-        var bigheight = $(".bigimg").get(0).offsetHeight
+        var bigwidth = $(".bigimg").width()
+        var bigheight = $(".bigimg").height()
         var scaley = 'x'
         var scalex = 'y'
         if (isNaN(scalex) | isNaN(scaley)) {
-          scalex = Math.round(bigwidth / imageWidth)
-          scaley = Math.round(bigheight / imageHeight)
+          scalex = bigwidth / imageWidth
+          scaley = bigheight / imageHeight
         }
         mouse = new MouseEvent(e)
         var scrolly = mouse.y - imageTop -imageBorderTopWidth - imagePaddingTopWidth - ($("span.zxx_image_zoom_div").height()*1/scaley)/2
-        var scrollyMax = imageHeight - ($("span.zxx_image_zoom_div").height()*1/scaley)
+        var scrollyMax = imageHeight - $("span.zxx_image_zoom_div").height()/scaley
         $("span.zxx_image_zoom_div").get(0).scrollTop = Math.min(scrollyMax, scrolly)*scaley
         var scrollx = mouse.x - imageLeft  - imageBorderLeftWidth - imagePaddingLeftWidth - ($("span.zxx_image_zoom_div").width()*1/scalex)/2
-        var scrollxMax = imageWidth - ($("span.zxx_image_zoom_div").width()*1/scalex)
+        var scrollxMax = imageWidth - ($("span.zxx_image_zoom_div").width()/scalex)
         $("span.zxx_image_zoom_div").get(0).scrollLeft = Math.min(scrollxMax, scrollx)*scalex
       })
     }, function () {
