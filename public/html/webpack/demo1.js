cff26a2dccf9c956b7207fecf9234305d4e82c70
moyumin <moyumin@simuwang.com>
2019-09-03 19:18:39 +0800

    添加兼容性代码

---
+++ public/html/webpack/demo1.js
@@ -0,0 +1,77 @@
+(function (modules) { // webpackBootstrap
+                      // The module cache
+
+  var installedModules = {};
+
+  // The require function
+
+  function __webpack_require__(moduleId) {
+
+    // Check if module is in cache
+
+    if (installedModules[moduleId]) {
+
+      return installedModules[moduleId].exports;
+
+    }
+    // Create a new module (and put it into the cache)
+
+    var module = installedModules[moduleId] = {
+      i: moduleId,
+      l: false,
+      exports: {}
+
+    };
+
+    // Execute the module function
+
+    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+    // Flag the module as loaded
+
+    module.l = true;
+
+    // Return the exports of the module
+
+    return module.exports;
+
+  }
+
+
+// 省略n个__webpack_require__上挂载的功能函数
+
+  // Load entry module and return exports
+
+  return __webpack_require__(__webpack_require__.s = 0);
+
+})
+/************************************************************************/
+([
+  /* 0 */
+  /***/ (function (module, exports, __webpack_require__) {
+
+// import message from './message.js';
+
+// export const msg = message
+// export default msgEntry = message + '!'
+
+    var c = __webpack_require__(1)
+    exports.default = c
+
+
+    /***/
+  }),
+  /* 1 */
+  /***/ (function (module, exports) {
+
+// export const name = 'world';
+    let c1 = 'c1'
+    let c2 = 'c2'
+    module.exports = {
+      c1,
+      c2,
+    }
+
+    /***/
+  })
+]);
